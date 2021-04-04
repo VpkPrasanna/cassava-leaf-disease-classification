e5a6525d0974c5fd87ff574b08206eb2933efc66
p-s-vishnu <psvishnu.it13@gmail.com>
2021-04-04 16:14:32 +0530

    update dataset.py; remove pylint

---
+++ cassava/dataset.py
@@ -0,0 +1,53 @@
+""" Image dataset definition
+"""
+
+import config
+import cv2
+import torch
+from torch.utils.data import Dataset
+
+
+class TrainDataset(Dataset):
+    """Train dataset class"""
+
+    def __init__(self, df, transform=None):
+        self.df = df
+        self.file_names = df["image_id"].values
+        self.labels = df["label"].values
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        file_name = self.file_names[idx]
+        file_path = f"{config.TRAIN_PATH}/{file_name}"
+        image = cv2.imread(file_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        if self.transform:
+            augmented = self.transform(image=image)
+            image = augmented["image"]
+        label = torch.tensor(self.labels[idx]).long()
+        return image, label
+
+
+class TestDataset(Dataset):
+    """Test dataset class"""
+
+    def __init__(self, df, transform=None):
+        self.df = df
+        self.file_names = df["image_id"].values
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        file_name = self.file_names[idx]
+        file_path = f"{config.TEST_PATH}/{file_name}"
+        image = cv2.imread(file_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        if self.transform:
+            augmented = self.transform(image=image)
+            image = augmented["image"]
+        return image
