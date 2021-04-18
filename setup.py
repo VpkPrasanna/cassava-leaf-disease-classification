951ffb894d2d78fc639ff85aa59f6c28ef227dde
p-s-vishnu <psvishnu.it13@gmail.com>
2021-04-18 17:24:19 +0530

    feat#17: Fixed requirements bug pypi

--- setup.py
+++ setup.py
@@ -7,30 +7,26 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-def install_requires():
-    required = None
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    try:
-        with open(os.path.join(current_dir, "requirements.txt"), encoding="utf-8") as f:
-            required = f.read().split("\n")
-    except FileNotFoundError:
-        required = []
-    return required
-
-
 setup(
     name="cassava_classifier",
     version="0.0.1",
     author="Prasanna Kumar, PS Vishnu",
-    author_email="vpkpypi@gmail.com, psvpypi@gmail.com",
-    description=("Casssava leaf disease classification using Deep neural network in Pytorch"),
+    description="Cassava leaf disease classification using Deep neural network in Pytorch",
     long_description_content_type="text/markdown",
     long_description=read("README.md"),
     license="MIT",
     keywords=["image classification", "leaf disease classifier", "pytorch"],
     url="https://github.com/p-s-vishnu/cassava-leaf-disease-classification",
     packages=find_packages(exclude=["tests", "docs", "images"]),
-    install_requires=install_requires(),
+    install_requires=[
+        "albumentations==0.5.2",
+        "apex==0.9.10dev",
+        "opencv-python==4.5.1.48",
+        "pandas==1.2.3",
+        "scikit_learn==0.24.1",
+        "timm==0.4.5",
+        "torch==1.8.1",
+    ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
